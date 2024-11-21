9f3d1c17f52535cbc2c6d6ba35e9f594bdd3ecff
EsanDipankar <esandipankar@gmail.com>
2024-11-21 14:43:36 +0530

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Anagram {
+    public static void main(String args[]){
+        String A="service";
+        String B="Request";
+        char[] arr= A.toCharArray();
+
+        
+
+
+    }
+}
