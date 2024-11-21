c7c1cd2f0ab759452b42eb3f02323bd7599bd66f
EsanDipankar <esandipankar@gmail.com>
2024-11-21 16:31:02 +0530

    Add files via upload

--- Anagram.java
+++ Anagram.java
@@ -2,11 +2,24 @@ import java.util.*;
 public class Anagram {
     public static void main(String args[]){
         String A="service";
-        String B="Request";
+        String B="request";
         char[] arr= A.toCharArray();
-
+        char[] Brr= B.toCharArray();
+        int[] mainArr= new int[26];
         
+        for(int i=0;i<A.length();i++){
+            mainArr[arr[i]-'a']++;
+        }
+        for(int i=0;i<B.length();i++){
+                mainArr[Brr[i]-'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (mainArr[i] != 0) {
+                System.out.println((char) (i + 'a') + " --> " + mainArr[i]);
+            }
+        }
 
+      
 
     }
 }
